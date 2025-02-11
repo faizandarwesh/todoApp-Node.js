cabab6d077a5659148a9bf06f46b4cb18390acb3
FaizanGT <faizan.darwesh@genetechsolutions.com>
2025-02-11 11:11:58 +0500

    Connect the app to port using env and fix some connection issues

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
+import app from "./app.js"
 
 dotenv.config({
     path : "./env"
 });
 
+console.log(`Testing env : ${process.env.API_KEY}`);
+
 app.listen(process.env.PORT || 3000,()=> {
-    console.log(`App is listening to port :  ${process.env.PORT}`);
+    console.log(`App is listening to port : ${process.env.PORT}`);
 });
