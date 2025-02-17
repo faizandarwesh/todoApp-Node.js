1be2d3c78313924a075e75c916592a8aca449cd9
FaizanGT <faizan.darwesh@genetechsolutions.com>
2025-02-17 18:21:32 +0500

    Add express-validator for handling server side validation for registration process

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,18 +1,28 @@
 import mongoose from "mongoose";
 import { User } from "../models/user.model.js";
 import { STATUS_CODES } from "../constants.js";
+import {validationResult} from "express-validator";
 
 export const login = async (req, res) => {};
 
 export const register = async (req, res) => {
   try {
+
+    const validationErrors = validationResult(req);
+    if(!validationErrors.isEmpty()){
+        return res.status(STATUS_CODES.BAD_REQUEST).json({
+            message : "Something went wrong",
+            errors : validationErrors.array()
+        })
+    }
+
     const { username, email, password } = req.body;
 
-    if ([username, email, password].some((field) => field?.trim() === "")) {
-      return res.status(STATUS_CODES.BAD_REQUEST).json({
-        message: "Please fill the required fields",
-      });
-    }
+    // if ([username, email, password].some((field) => field?.trim() === "")) {
+    //   return res.status(STATUS_CODES.BAD_REQUEST).json({
+    //     message: "Please fill the required fields",
+    //   });
+    // }
 
     const isUserExist = await User.findOne({
       $or: [
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { login, register } from "../controllers/user.controller.js";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/login",login);
-router.post("/register",register);
+router.post("/login", login);
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email address"),
+    body("username").notEmpty().withMessage("Username is a required field"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters long"),
+  ],
+  register
+);
 
 export default router;
