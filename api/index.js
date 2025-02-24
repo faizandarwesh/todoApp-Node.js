eb6cb9042a363fdea9cb09d5cd967be9d17d5e66
FaizanGT <faizan.darwesh@genetechsolutions.com>
2025-02-24 17:13:37 +0500

    Just replace code from api/index.js to test code for test deployment

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-import app from "../index.js";
+// import app from "../index.js";
 
-export default app;
+// export default app;
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
